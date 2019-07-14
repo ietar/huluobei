d7833a275001f0f65eabc1e5e69a3008114b9ccf
ietar <410473517@qq.com>
2019-07-14 16:34:23 +0800

    mysql password reset

--- static/js/index1_ajax.js
+++ static/js/index1_ajax.js
@@ -1,12 +1,12 @@
 function func1(){
-    form = document.getElementById('form1');
-    fd = new FormData(form);
+    var form = document.getElementById('form1');
+    var fd = new FormData(form);
     var xhr = new XMLHttpRequest();
     xhr.open('post', '/shit/');
     xhr.send(fd);
     xhr.onreadystatechange = function(){
-        if(xhr.readyState == 4){
-            if (xhr.status == 200 || xhr.status == 304){
+        if(4 === xhr.readyState){
+            if (xhr.status === 200 || xhr.status === 304){
                 var ret = JSON.parse(xhr.responseText);
                 document.getElementById('d1').innerHTML = ret.username;
                 document.getElementById('d2').innerHTML = ret.email;
--- zhihu_django/settings.py
+++ zhihu_django/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test6',
         'USER': 'root',
-        'PASSWORD': 'qiurilushi1',
+        'PASSWORD': '132132',
         'HOST': 'localhost',
         'PORT': '3306',
     }
