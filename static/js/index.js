de683f50483c19dc77e9e8303e1d8bc2578577f9
ietar <410473517@qq.com>
2019-09-06 18:37:41 +0800

    index

--- static/js/index.js
+++ static/js/index.js
@@ -1,27 +1,36 @@
 function f1(){
-    var form = document.getElementById('drawcards');
-    var fd = new FormData(form);
-    var xhr = new XMLHttpRequest();
+    let form = document.getElementById('drawcards');
+    let fd = new FormData(form);
+    let xhr = new XMLHttpRequest();
     xhr.open('post', '/drawcards/');
     xhr.send(fd);
     xhr.onreadystatechange = function(){
         if(4 === xhr.readyState){
             if (xhr.status === 200 || xhr.status === 304){
-                var ret = JSON.parse(xhr.responseText);
+                let ret = JSON.parse(xhr.responseText);
                 document.getElementById('draw_result').innerHTML = ret.length;
             }
         }
     }
 }
 function f2(){
-    var iframe = document.getElementById('iframe1');
+    let iframe = document.getElementById('iframe1');
     iframe.src = "/drawcards/";
     iframe.width = 660;
     iframe.height = 150;
 }
 function f3(){
-    var iframe = document.getElementById('iframe1');
+    let iframe = document.getElementById('iframe1');
     iframe.src = "";
     iframe.width = 0;
     iframe.height = 0;
 }
+$(function () {
+    let works_button = $('div.header>div.container>div').eq(0);
+    console.log('here:', works_button);
+    let works = $('div.works');
+    console.log("works:", works);
+    works_button.click(function () {
+        works.slideToggle();
+})
+});
