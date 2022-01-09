2ae16048a4b5488f37b3179df5271b196ab70790
ietar <410473517@qq.com>
2022-01-09 18:54:24 +0800

    fix

--- crawlers/views.py
+++ crawlers/views.py
@@ -37,7 +37,7 @@ def login_check(request) -> ret_u:
 def get_chapter(request):
     # 获取简介 自用
     u = request.user
-    if not u.is_staff or not u.is_superuser:
+    if not u.is_staff and not u.is_superuser:
         return HttpResponse('非管理员 无操作权限')
 
     try:
@@ -79,7 +79,7 @@ def get_content(request):
     # return HttpResponse('lock')
     # u = login_check(request)
     u = request.user
-    if not u.is_staff or not u.is_superuser:
+    if not u.is_staff and not u.is_superuser:
         return HttpResponse('非管理员 无操作权限')
     get_dict = request.GET
     # print(get_dict, dir(get_dict))
