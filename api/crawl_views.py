bd258a412501f8716f50be7adab72b838aeab59d
ietar <410473517@qq.com>
2022-01-13 14:38:08 +0800

    try async

--- api/crawl_views.py
+++ api/crawl_views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 
 from crawlers.models import Book
 from utils.any import logger
-from utils.async_functions import async_get_content
+from utils.async_functions import async_get_content, sync_get_content
 from utils.default_data import n_data
 
 
@@ -20,9 +20,11 @@ def get_content_view(request):
 
     try:
         # 校验
+
         u = request.user
         if not u.is_staff and not u.is_superuser:
             raise Exception('非管理员 无操作权限')
+
         get_dict = request.GET
         if not get_dict:
             raise Exception('id(book_id), prefix(biqooge的url prefix_book_id), target(爬几章)')
@@ -38,14 +40,14 @@ def get_content_view(request):
     except ObjectDoesNotExist:
         res['result'] = False
         res['msg'] = '还没这本书的简介 先去get_chapter'
-        return JsonResponse(res, status=400)
+        return JsonResponse(res, status=400, json_dumps_params={'ensure_ascii': False})
 
     except Exception as e:
         traceback.print_exc()
         logger.error(e)
         res['result'] = False
         res['msg'] = str(e)
-        return JsonResponse(res, status=400)
+        return JsonResponse(res, status=400, json_dumps_params={'ensure_ascii': False})
 
     try:
         if book1.using:
@@ -53,11 +55,11 @@ def get_content_view(request):
         else:
             book1.using = True
             book1.save()
-        await async_get_content(book_id=book_id, current_chapter=book1.current, target=target, prefix=prefix)
-        book1.using = False
-        book1.save()
+        async_get_content(book_id=book_id, current_chapter=book1.current, target=target, prefix=prefix)
+
         res['msg'] = f'已提交,当前待爬取章节数为 {book1.current + target}, 用时{round(time.time() - t1, 2)}秒'
-        return JsonResponse(res)
+        return JsonResponse(res, json_dumps_params={'ensure_ascii': False})
+        # return JsonResponse(res)
 
     except Exception as e:
         traceback.print_exc()
@@ -66,4 +68,4 @@ def get_content_view(request):
         book1.save()
         res['result'] = False
         res['msg'] = str(e)
-        return JsonResponse(res, status=400)
+        return JsonResponse(res, status=400, json_dumps_params={'ensure_ascii': False})
--- utils/async_functions.py
+++ utils/async_functions.py
@@ -27,6 +27,37 @@ def async_get_content(book_id, current_chapter, target, prefix):
             content=content,
             update_time=timezone.now()
         )
+        print(content)
+        print(new_content.id)
+        new_content.save()
+        count += 1
+        book1.current = book1.current + 1
+        book1.save()
+
+    book1.using = False
+    book1.save()
+    return book1.current
+
+
+def sync_get_content(book_id, current_chapter, target, prefix):
+    book1 = Book.objects.get(book_id=book_id)
+    chapters = biqooge.get_chapters(
+        book_id=book_id, current_chapter=current_chapter, chapter_count=target, prefix=prefix)
+    count = 0
+
+    for chapter in chapters:
+        chapter_name = chapter.a.text.strip()
+        url1 = chapter.a['href']
+        url = biqooge.root_url + url1
+        content = biqooge.get_content(content_url=url)
+        # content = sync_to_async(biqooge.get_content)(content_url=url)
+        new_content = BookContent.objects.create(
+            book_name=book1,
+            chapter_count=book1.current,
+            chapter=chapter_name,
+            content=content,
+            update_time=timezone.now()
+        )
         new_content.save()
         count += 1
         book1.current = book1.current + 1
